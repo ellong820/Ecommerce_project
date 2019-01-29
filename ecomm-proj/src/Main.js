178d122690565121663dc371313777f6d4da962a
Eric Long <ellong820@gmail.com>
2019-01-28 22:52:22 -0800

    added Header

---
+++ ecomm-proj/src/Main.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ELO <small>Simple and minimalistic</small></h1>
+        <header className="navaigation">
+          <ul>
+            <li><a href="#">New</a></li>
+            <li><a href="#">Women</a></li>
+            <li><a href="#">Men</a></li>
+            <li><a href="#">Kids</a></li>
+            <li><a href="#">Sale</a></li>
+            <li><a href="#">New</a></li>
+          </ul>
+        </header>
+        <div className="banner">
+          <img src="https://static.zara.net/photos///mkt/spots/ss19-campaign-woman/home-teaser//desktop-whitebackgrouindd.st.jpg?1548358944991"></img>
+        </div>
+
+        <div className="content">
+
+
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
